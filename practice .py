bf3277d1f1fb059c35adb70edc66d9f7e0805035
samarth sable <samarthsable0005@gmail.com>
2025-02-05 15:21:23 +0530

    code update day1

--- practice .py
+++ practice .py
@@ -377,3 +377,19 @@ elif a < b < c:
     print("increasing order")
 else:
     print("number is neither increasing or decreasing")
+
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year % 4 == 0:
+        if year % 100 ==0:
+            if year % 400 == 0:
+                leap = True
+        else:
+            leap= True
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
